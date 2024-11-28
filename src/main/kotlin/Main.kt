fc2801ec43f4b332066dc6becb6df9c99c9cfb90
gabrielimartins7 <martins.gabrieli.07.almeida@outlook.com>
2024-11-28 14:39:47 -0300

    Tratando erros e refatoração

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,12 +4,20 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
 
 import com.google.gson.Gson
+import java.util.Scanner
 
 
 fun main() {
+    val reading = Scanner(System.`in`)
+    println("Enter a game code to search: ")
+
+    val search = reading.nextLine()
+
+    val address = "https://www.cheapshark.com/api/1.0/games?id=$search"
+
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create(address))
         .build()
     val response = client
         .send(request, BodyHandlers.ofString())
@@ -18,7 +26,12 @@ fun main() {
     println(json)
 
     val gson = Gson()
-    val myGame = gson.fromJson(json, InfoGame::class.java)
+    val InfoMyGame = gson.fromJson(json, InfoGame::class.java)
+
+    val myGame = Game(
+            InfoMyGame.info.title,
+            InfoMyGame.info.thumb
+    )
 
     println(myGame)
 }
